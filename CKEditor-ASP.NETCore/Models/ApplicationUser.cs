7c3a6be65add5d9825dc5415380040b52e3b467d
aLLUPS <73pawara@gmail.com>
2021-04-13 23:35:47 +0530

    Feat(configure CKEditor): Complete project
    1. add new models
    2. create migrations
    3. add ckeditor library
    4. add controllers and views
    5. modify view and import ckeditor

---
+++ CKEditor-ASP.NETCore/Models/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CKEditor_ASP.NETCore.Models
+{
+    public partial class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser()
+        {
+            UserArticles = new HashSet<UserArticle>();
+        }
+
+        public virtual ICollection<UserArticle> UserArticles { get; set; }
+    }
+}
